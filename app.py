e294c5b08156bf0bf0798db1797f60ea6e9eba98
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2024-12-30 15:12:39 -0500

    ajustes en el kernel para correr el notebook

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-df = pd.read_csv('vehuicle.csv')
+df = pd.read_csv('vehicles_us.csv')
+df
+
+st.title('Vehicle dataset')
+st.write('This is a simple example of a Streamlit app')
