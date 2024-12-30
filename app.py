abfc2a5edff51832e9a1024c1228dcf7b9b9b3f7
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2024-12-30 16:42:09 -0500

    Limpieza de datos de vehicles_us.csv

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import pandas as pd
 import plotly_express as px
 
 # Load the data
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv') 
+df.info()
+
+#Cleaning the data
+df = df.dropna()
+df['model_year'] = df['model_year'].astype(int)
+df['date_posted'] = pd.to_datetime(df['date_posted'])
+
 # Titles
 st.title('Vehicle dataset')
 st.write('This is a simple example of a Streamlit app')
