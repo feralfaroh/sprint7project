6d7d5ba0860c1ca0e621678587303911d35efd5c
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2024-12-30 15:51:09 -0500

    Se agregaron los archivos de la base de datos y se modifico el archivo app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
+# Load the data
 df = pd.read_csv('vehicles_us.csv')
-df
-
+# Titles
 st.title('Vehicle dataset')
 st.write('This is a simple example of a Streamlit app')
