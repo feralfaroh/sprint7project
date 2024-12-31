e941e44f002c478aaf407382150e72ed7b5a6b49
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2024-12-31 08:57:50 -0500

    Prueba de 2dos graficos

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
 
 # Load the data
 df = pd.read_csv('vehicles_us.csv') 
 df.info()
 
-#Cleaning the data
+# Cleaning the data
 df = df.dropna()
 df['model_year'] = df['model_year'].astype(int)
 df['date_posted'] = pd.to_datetime(df['date_posted'])
 
 # Titles
-st.title('Vehicle dataset')
+st.title('Vehicle Dataset')
 st.write('Estudiante FERNANDO JESUS ALFARO HERNANDEZ')
 
 # Show the data 
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Construir histograma')  # Crear un botón
         
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
+if hist_button:  # Al hacer clic en el botón
+    # Escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
-    # crear un histograma
+    # Crear un histograma
     fig = px.histogram(df, x="odometer")
         
-    # mostrar un gráfico Plotly interactivo
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+    # Título
+    st.write('Gráfico de barras')
+
+    # Gráfico de barras
+    gb = df.groupby(['model_year', 'fuel'])['odometer'].sum().reset_index()
+    fig = px.bar(gb, x="model_year", y="odometer", color="fuel", barmode="stack")
+    
+    # Mostrar el gráfico de barras en la interfaz de Streamlit
     st.plotly_chart(fig, use_container_width=True)
