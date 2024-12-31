04d33a9c96e18cd7f7500c2ca2024239e2bc7993
Fernando Alfaro <122326590+feralfaroh@users.noreply.github.com>
2024-12-30 19:13:44 -0500

    se agrego boton de prueba

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ df['date_posted'] = pd.to_datetime(df['date_posted'])
 st.title('Vehicle dataset')
 st.write('Estudiante FERNANDO JESUS ALFARO HERNANDEZ')
 
-# Show the data
-df
+# Show the data 
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(df, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
